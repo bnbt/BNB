c1994f64c29b50309ac06ab637423c66c8faf5f8
Ionut <johan@johan.ro>
2016-03-22 23:20:05 +0200

    Add CMake. Start reorganizing main app

--- BNB_Main.ino
+++ BNB_Main.ino
@@ -4,67 +4,88 @@
 #include "lib/Adafruit_LiquidCrystal.h"
 #include "lib/ESP8266wifi.h"
 
-#define sw_serial_rx_pin 7 //  Connect this pin to TX on the esp8266
-#define sw_serial_tx_pin 8 //  Connect this pin to RX on the esp8266
-#define esp8266_reset_pin 12 // Connect this pin to CH_PD on the esp8266, not reset. (let reset be unconnected)
 
-SoftwareSerial swSerial(sw_serial_rx_pin, sw_serial_tx_pin);
+/*************************
+ * DECLARATIONS
+ ************************/
+
+// CONSTANTS
+#define ESP_RX_PIN 7 //  Connect this pin to TX on the esp8266
+#define ESP_TX_PIN 8 //  Connect this pin to RX on the esp8266
+#define ESP_RESET_PIN 12 // Connect this pin to CH_PD on the esp8266, not reset. (let reset be unconnected)
+
+#define RFID_D0 2
+#define RFID_D0_INT 0
+#define RFID_D1 3
+#define RFID_D1_INT 1
+
+#define BUTTON_PIN 6
+#define RED_PIN 9
+#define GREEN_PIN 10
+#define BLUE_PIN  11
+
+#define LCD_ADDR 0x20
+
+// GLOBAL VARIABLES
+SoftwareSerial swSerial(ESP_RX_PIN, ESP_TX_PIN);
+WIEGAND rfid;
+Adafruit_LiquidCrystal lcd(LCD_ADDR);
 
 // the last parameter sets the local echo option for the ESP8266 module..
-// ESP8266wifi wifi(swSerial, swSerial, esp8266_reset_pin, Serial);//adding Serial enabled local echo and wifi debug
+// ESP8266wifi wifi(swSerial, swSerial, ESP_RESET_PIN, Serial);//adding Serial enabled local echo and wifi debug
 
 String inputString;
 boolean stringComplete = false;
 unsigned long nextPing = 0;
 
-WIEGAND wg;
-Adafruit_LiquidCrystal lcd(0x20);
 
-#define buttonPin 6
-
-
-#define redPin 9
-#define greenPin 10
-#define bluePin  11
+///////////////////////////////
+// LCD
+///////////////////////////////
+void lcd_setup() {
+    lcd.begin(16, 2);
+    lcd.setBacklight(HIGH);
+    // Print a message to the LCD.
+    lcd.print("hello, world!");
+}
 
 void setup() {
-  lcd_setup();
+    lcd_setup();
 
-  pinMode(esp8266_reset_pin, OUTPUT);
-  digitalWrite(esp8266_reset_pin, LOW);
-   delay(500);
-   digitalWrite(esp8266_reset_pin, HIGH); 
-swSerial.begin(9600);
+    pinMode(ESP_RESET_PIN, OUTPUT);
+    digitalWrite(ESP_RESET_PIN, LOW);
+    delay(500);
+    digitalWrite(ESP_RESET_PIN, HIGH);
+    swSerial.begin(9600);
 
-  
-  Serial.begin(9600);  
-  Serial.println("Ready");
+
+    Serial.begin(9600);
+    Serial.println("Ready");
 }
 
 void loop() {
 //  led_loop();
 //  esp_loop();
-byte x;
+    byte x;
 
- if ( swSerial.available() )   { 
-      x = swSerial.read();
-      Serial.write(x);  
-    } else if ( Serial.available() ) { 
-      x = Serial.read();
-      swSerial.write(x);
-//      Serial.write(x); 
+    if (swSerial.available()) {
+        x = (byte) swSerial.read();
+        Serial.write(x);
+    } else if (Serial.available()) {
+        x = (byte) Serial.read();
+        swSerial.write(x);
     }
 }
 
 ////////////////////////// ESP test 2
 
 void esp_test2() {
-  swSerial.begin(115200);
-  pinMode(esp8266_reset_pin, OUTPUT);
-  digitalWrite(esp8266_reset_pin, LOW);
-   delay(500);
-   digitalWrite(esp8266_reset_pin, HIGH); 
-   char buf[16] = {'\0'};
+    swSerial.begin(115200);
+    pinMode(ESP_RESET_PIN, OUTPUT);
+    digitalWrite(ESP_RESET_PIN, LOW);
+    delay(500);
+    digitalWrite(ESP_RESET_PIN, HIGH);
+    char buf[16] = {'\0'};
     strcpy_P(buf, (char *) "AT+CWLAP");
     swSerial.print(buf);
     swSerial.println();
@@ -74,37 +95,36 @@ void esp_test2() {
 ///////////////////////////////
 // LED
 ///////////////////////////////
-void led_setup(){
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);  
+void led_setup() {
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
 }
 
-void led_loop() {
-  setColor(255, 0, 0);  // red
-  delay(1000);
-  setColor(0, 255, 0);  // green
-  delay(1000);
-  setColor(0, 0, 255);  // blue
-  delay(1000);
-  setColor(255, 255, 0);  // yellow
-  delay(1000);  
-  setColor(80, 0, 80);  // purple
-  delay(1000);
-  setColor(0, 255, 255);  // aqua
-  delay(1000);
-}
-
-void setColor(int red, int green, int blue)
-{
+void setColor(int red, int green, int blue) {
 //  #ifdef COMMON_ANODE
 //    red = 255 - red;
 //    green = 255 - green;
 //    blue = 255 - blue;
 //  #endif
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);  
+    analogWrite(RED_PIN, red);
+    analogWrite(GREEN_PIN, green);
+    analogWrite(BLUE_PIN, blue);
+}
+
+void led_loop() {
+    setColor(255, 0, 0);  // red
+    delay(1000);
+    setColor(0, 255, 0);  // green
+    delay(1000);
+    setColor(0, 0, 255);  // blue
+    delay(1000);
+    setColor(255, 255, 0);  // yellow
+    delay(1000);
+    setColor(80, 0, 80);  // purple
+    delay(1000);
+    setColor(0, 255, 255);  // aqua
+    delay(1000);
 }
 
 ///////////////////////////////
@@ -112,56 +132,45 @@ void setColor(int red, int green, int blue)
 ///////////////////////////////
 
 void button_setup() {
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
-  digitalWrite(buttonPin, HIGH);
+    // initialize the pushbutton pin as an input:
+    pinMode(BUTTON_PIN, INPUT);
+    digitalWrite(BUTTON_PIN, HIGH);
 }
 
 void button_loop() {
-  int state = digitalRead(buttonPin);
-  if (state == HIGH) {
-    lcd.setCursor(0, 0);
-    lcd.print("* ");
-  }
-  if (state == LOW) {
-    lcd.setCursor(0, 0);
-    lcd.print(" +");
-  }
+    int state = digitalRead(BUTTON_PIN);
+    if (state == HIGH) {
+        lcd.setCursor(0, 0);
+        lcd.print("* ");
+    }
+    if (state == LOW) {
+        lcd.setCursor(0, 0);
+        lcd.print(" +");
+    }
 }
 
-
-///////////////////////////////
-// LCD
-///////////////////////////////
-void lcd_setup() {
-  lcd.begin(16, 2);
-  lcd.setBacklight(HIGH);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
-}
 
 ///////////////////////////////
 // Wiegand
 ///////////////////////////////
 
 void wiegand_setup() {
-  wg.begin();
+    rfid.begin(RFID_D0, RFID_D0_INT, RFID_D1, RFID_D1_INT);
 }
 
 void wiegand_loop() {
-   if(wg.available())
-  {
-    Serial.print("Wiegand HEX = ");
-    Serial.print(wg.getCode(),HEX);
-    lcd.setCursor(0, 0);
-    lcd.print(wg.getCode(),HEX);
-    Serial.print(", DECIMAL = ");
-    Serial.print(wg.getCode());
-     lcd.setCursor(0, 1);
-    lcd.print(wg.getCode()); 
-    Serial.print(", Type W");
-    Serial.println(wg.getWiegandType());    
-  }
+    if (rfid.available()) {
+        Serial.print("Wiegand HEX = ");
+        Serial.print(rfid.getCode(), HEX);
+        lcd.setCursor(0, 0);
+        lcd.print(rfid.getCode(), HEX);
+        Serial.print(", DECIMAL = ");
+        Serial.print(rfid.getCode());
+        lcd.setCursor(0, 1);
+        lcd.print(rfid.getCode());
+        Serial.print(", Type W");
+        Serial.println(rfid.getWiegandType());
+    }
 }
 
 
@@ -240,7 +249,7 @@ void wiegand_loop() {
 //    nextPing = millis() + 10000;
 //  }
 //  
-  //If you want do disconnect from the server use:
-  // wifi.disconnectFromServer();
+//If you want do disconnect from the server use:
+// wifi.disconnectFromServer();
 //}
 
--- README.md
+++ README.md
@@ -5,3 +5,21 @@ Big N Button
 ```
 AT+CIOBAUD=9600
 ```
+
+# PIN Configuration
+### ESP WIFI
+- ESP_RX_PIN    7   ->  ESP TX
+- ESP_TX_PIN    8   ->  ESP RX
+- ESP_RESET_PIN 12  -> ESP CH_PD
+
+### RFID reader
+- RFID_D0   2
+- RFID_D1   3
+
+### BUTTON
+- BUTTON_PIN    6
+
+### RGB LED
+- RED_PIN       9
+- GREEN_PIN     10
+- BLUE_PIN      11
